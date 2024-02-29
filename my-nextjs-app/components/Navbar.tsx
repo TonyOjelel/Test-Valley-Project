2896551c84b3c6d65f511d20ca60205bea048e26
Tony Ojelel <tonyojelel@gmail.com>
2024-02-29 20:36:55 +0300

    8th commit

--- my-nextjs-app/components/Navbar.tsx
+++ my-nextjs-app/components/Navbar.tsx
@@ -1,24 +1,31 @@
 // components/Navbar.tsx
-import React from 'react';
+import React from "react";
 
 const Navbar: React.FC = () => {
-    return (
-        <nav className="navbar">
-            <div className="logo">Testvalley</div>
-            <div className="menu-icon">☰</div>
-            <div className="nav-links">
-                <a href="#">카테고리</a>
-            </div>
-            <div className="search-bar">
-                <input type="text" placeholder="🔍 구매할지 망설이고 있다면" />
-                {/* <button>🔍</button> */}
-            </div>
-            <div className="icons">
-                <span>50%</span>
-                <a href="#">로그인 / 가입하기</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <div className="logo">
+          <img
+            src="https://www.testvalley.kr/logo/logo-new.svg"
+            alt="Testvalley Logo"
+          />
+        </div>
+      </div>
+      <div className="menu-icon">☰</div>
+      <div className="nav-links">
+        <a href="#">카테고리</a>
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="🔍 구매할지 망설이고 있다면" />
+        {/* <button>🔍</button> */}
+      </div>
+      <div className="icons">
+        <span>50%</span>
+        <a href="#">로그인 / 가입하기</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
